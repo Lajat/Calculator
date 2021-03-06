8dbec409611638b54bd89fe14a897a806464e21f
Lajat <51463030+Lajat@users.noreply.github.com>
2021-03-06 21:25:51 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,38 +1,51 @@
-import React from 'react'
-import update from 'react-addons-update'
-import math from 'mathjs'
+import React, { Component } from 'react';
+import update from 'react-addons-update';
+// import update from 'immutability-helper';
+import math from 'mathjs';
 import './App.css';
+import Display from './components/Display';
+import Button from './components/Button';
+import Buttons from './components/Buttons';
 
-var App = React.createClass({
-  getInitialState: function() {
-    return ({
-      operations: []
-    });
-  },
-  calculateOperations: function() {
-    var result = this.state.operations.join('');
-    if(result){
-      result = String(math.eval(result));
-      this.setState({ operations : [result] });
+class App extends Component {
+  constructor() {
+    super()
+    this.state = { operations: [] }
+  }
+
+  calculateOperations = () => {
+    let result = this.state.operations.join('')
+    if (result) {
+      result = math.eval(result)
+      result = math.format(result, { precision: 14 })
+      result = String(result)
+      this.setState({
+        operations: [result],
+      })
     }
-    
-  },
-  handleClick: function(e) {
-    var value = e.target.getAttribute('data-value');
+  }
+  handleClick = e => {
+    const value = e.target.getAttribute('data-value')
     switch (value) {
       case 'clear':
-        this.setState({ operations: []});
-        break;
+        this.setState({
+          operations: [],
+        })
+        break
       case 'equal':
-        this.calculateOperations();
-        break;
+        this.calculateOperations()
+        break
       default:
-        var newOperations = update(this.state.operations, {$push: [value]});
-        this.setState({operations: newOperations});
-        break;
+        const newOperations = update(this.state.operations, {
+          $push: [value],
+        })
+        this.setState({
+          operations: newOperations,
+        })
+        break
     }
-  },
-  render: function() {
+  }
+  render() {
     return (
       <div className="App">
         <Display data={this.state.operations} />
@@ -42,57 +55,26 @@ var App = React.createClass({
           <Button onClick={this.handleClick} label="4" value="4" />
           <Button onClick={this.handleClick} label="1" value="1" />
           <Button onClick={this.handleClick} label="0" value="0" />
-          
+
           <Button onClick={this.handleClick} label="/" value="/" />
           <Button onClick={this.handleClick} label="8" value="8" />
           <Button onClick={this.handleClick} label="5" value="5" />
           <Button onClick={this.handleClick} label="2" value="2" />
           <Button onClick={this.handleClick} label="." value="." />
-          
+
           <Button onClick={this.handleClick} label="x" value="*" />
           <Button onClick={this.handleClick} label="9" value="9" />
           <Button onClick={this.handleClick} label="6" value="6" />
           <Button onClick={this.handleClick} label="3" value="3" />
           <Button label="" value="null" />
-          
+
           <Button onClick={this.handleClick} label="-" value="-" />
           <Button onClick={this.handleClick} label="+" size="2" value="+" />
           <Button onClick={this.handleClick} label="=" size="2" value="equal" />
         </Buttons>
       </div>
-    );
-  }
-});
-
-var Display = React.createClass({
-  render: function() {
-    var string = this.props.data.join('');
-    return (
-      <div className="Display">
-        {string}
-      </div>
-    );
-  }
-});
-
-var Buttons = React.createClass({
-  render: function() {
-    return (
-      <div className="Buttons">
-        {this.props.children}
-      </div>
     )
   }
-});
-
-var Button = React.createClass({
-  render: function() {
-    return (
-      <div onClick={this.props.onClick} className="Button" data-size={this.props.size} data-value={this.props.value}>
-      {this.props.label}  
-      </div>
-    )
-  }
-});
+}
 
 export default App;
